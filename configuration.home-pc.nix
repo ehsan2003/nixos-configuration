74bb558523050e9278f745e46647bebc6fdfd809
ehsan <ehsan2003.2003.382@gmail.com>
2023-04-04 19:54:27 +0330

    changes systemd-boot into grub

--- configuration.home-pc.nix
+++ configuration.home-pc.nix
@@ -3,8 +3,11 @@
   imports = [./common.nix];
   networking.hostName = "nixos-home-desktop"; # Define your hostname.
 
-  boot.loader.systemd-boot.enable = true;
-  
+  boot.loader.grub.device="nodev";
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber=true;
+
+
   home-manager.users.ehsan.programs.alacritty.settings.font.size=18;
   services.xserver.dpi = 120 ;
 }
