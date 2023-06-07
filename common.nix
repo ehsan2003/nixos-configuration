d0073a692f1b5629b1db3e8b40c92217a57fd196
ehsan <ehsan2003.2003.382@gmail.com>
2023-06-07 11:45:25 +0330

    adds v4l

--- common.nix
+++ common.nix
@@ -47,6 +47,27 @@ in {
   #   keyMap = "us";
   #   useXkbConfig = true; # use xkbOptions in tty.
   # };
+  # Make some extra kernel modules available to NixOS
+  boot.extraModulePackages = with config.boot.kernelPackages;
+    [ v4l2loopback.out ];
+
+  # Activate kernel modules (choose from built-ins and extra ones)
+  boot.kernelModules = [
+    # Virtual Camera
+    "v4l2loopback"
+    # Virtual Microphone, built-in
+    "snd-aloop"
+  ];
+
+  # Set initial kernel module settings
+  boot.extraModprobeConfig = ''
+    # exclusive_caps: Skype, Zoom, Teams etc. will only show device when actually streaming
+    # card_label: Name of virtual camera, how it'll show up in Skype, Zoom, Teams
+    # https://github.com/umlaeute/v4l2loopback
+    options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+  '';
+
+
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -280,5 +301,4 @@ in {
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
-
 }
