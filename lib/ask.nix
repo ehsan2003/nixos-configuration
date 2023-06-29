958c655d4ae7e8199ef9073fdf3d011070035103
ehsan <ehsan2003.2003.382@gmail.com>
2023-06-29 15:02:05 +0330

    chatgpt ask command

---
+++ lib/ask.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+with pkgs;
+writeShellApplication rec {
+  name = "ask";
+  runtimeInputs = [curl jq  ];
+  text = ''
+    data=$(echo "$@" | jq -R '{prompt:.,options:{}}')
+    curl 'https://chatbot.theb.ai/api/chat-process' \
+      -H 'content-type: application/json' \
+      -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36' \
+      --data-raw "$data" \
+      --compressed 2>/dev/null| jq '.delta' --join-output 
+  '';
+}
