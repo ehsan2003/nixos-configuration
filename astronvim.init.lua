a590c31af178523cf42bb05c9b94bf1b28ca8ab0
ehsan <ehsan2003.2003.382@gmail.com>
2023-08-12 11:14:54 +0330

    some changes in programming environment

--- astronvim.init.lua
+++ astronvim.init.lua
@@ -54,7 +54,7 @@ return {
 				},
 			},
 			disabled = { -- disable formatting capabilities for the listed language servers
-				-- "sumneko_lua",
+				"tsserver",
 			},
 			timeout_ms = 1000, -- default format timeout
 			-- filter = function(client) -- fully override the default formatting function
--- programming.nix
+++ programming.nix
@@ -24,6 +24,26 @@ in {
   };
   environment.systemPackages = with pkgs; [
     neovim
+    (vscode-with-extensions.override {
+        vscode = vscodium;
+        vscodeExtensions = with vscode-extensions; [
+          bbenoist.nix
+          ms-python.python
+          ms-azuretools.vscode-docker
+          ms-vscode-remote.remote-ssh
+          vscodevim.vim
+          rust-lang.rust-analyzer
+        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "remote-ssh-edit";
+            publisher = "ms-vscode-remote";
+            version = "0.47.2";
+            sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+          }
+        ];
+      })
+
+
     nodejs
     yarn
     python39
@@ -47,6 +67,7 @@ in {
     llvmPackages_15.clang-unwrapped
     lua-language-server
     stylua
+    pre-commit
   ];
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "ehsan" ];
