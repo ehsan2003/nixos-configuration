6279f49dd5eb43f4ea7a111c53f95b89fd6f5094
ehsan <ehsan2003.2003.382@gmail.com>
2023-11-11 16:42:39 +0330

    adds current config to iso

--- iso.nix
+++ iso.nix
@@ -3,12 +3,9 @@
     "${modulesPath}/installer/cd-dvd/installation-cd-graphical-base.nix"
     ./common.nix
   ];
-
-  environment.etc.proxy = {
-    enable = true;
-    mode = "0755";
-    source = /etc/proxy/main.sh;
-    target = "proxy/main.sh";
+  home-manager.users.ehsan.home.file.nixos-configuration = {
+    source = ./.;
+    target = "nixos-configuration";
   };
   isoImage.squashfsCompression = "gzip -Xcompression-level 1";
   isoImage.compressImage = false;
