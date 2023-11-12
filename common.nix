dfc02a916bf7c48f1e3a6bfd24afd4cfd1475a25
ehsan <ehsan2003.2003.382@gmail.com>
2023-11-12 22:12:36 +0330

    remove unnecessary comments

--- common.nix
+++ common.nix
@@ -1,10 +1,6 @@
 { home-manager, ... }:
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   imports = [
-    # Include the results of the hardware scan.
     ./media.nix
     ./programming
     ./gui
@@ -13,17 +9,6 @@
     ./free-net.nix
     home-manager.nixosModules.default
   ];
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
