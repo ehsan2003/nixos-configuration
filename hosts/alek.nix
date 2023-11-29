6270e9204d2c33a8fa2c84fd4f4f5c9b00a93904
ehsan <ehsan2003.2003.382@gmail.com>
2023-11-29 10:12:28 +0330

    adds nvidia for alek

--- hosts/alek.nix
+++ hosts/alek.nix
@@ -1,11 +1,28 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   imports = [ ./base.nix ];
   networking.hostName = "alek"; # Define your hostname.
   services.xserver.displayManager.setupCommands = "${pkgs.xorg.xrandr}/bin/xrandr --rate 60 &";
   services.tlp.enable = true;
-  environment.systemPackages=[
+  environment.systemPackages = [
     pkgs.jetbrains.idea-ultimate
     pkgs.brave
   ];
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 
 }
