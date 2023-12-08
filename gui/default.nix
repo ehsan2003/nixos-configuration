2076aad75609cfcf1d4d46aab9140e53a6fd438b
ehsan <ehsan2003.2003.382@gmail.com>
2023-12-08 10:11:37 +0330

    disable firefox stuff to get used to qutebrowser

--- gui/default.nix
+++ gui/default.nix
@@ -40,49 +40,49 @@
       alacritty = {
         enable = true;
       };
-      firefox = {
-        enable = true;
-        package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-          extraPolicies = {
-            CaptivePortal = false;
-            DisableFirefoxStudies = true;
-            DisablePocket = true;
-            DisableTelemetry = true;
-            DisableFirefoxAccounts = false;
-            NoDefaultBookmarks = true;
-            OfferToSaveLogins = true;
-            OfferToSaveLoginsDefault = true;
-            PasswordManagerEnabled = true;
-            FirefoxHome = {
-              Search = true;
-              Pocket = false;
-              Snippets = false;
-              TopSites = false;
-              Highlights = false;
-            };
-            UserMessaging = {
-              ExtensionRecommendations = false;
-              SkipOnboarding = true;
-            };
-          };
-        };
-        profiles.default = {
-          name = "default";
-          path = "default";
-          id = 0;
-
-          extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-            tridactyl
-            switchyomega
-          ];
-          isDefault = true;
-          search.default = "DuckDuckGo";
-          search.force = true;
-          settings = {
-            "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
-          };
-        };
-      };
+      # firefox = {
+      #   enable = true;
+      #   package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
+      #     extraPolicies = {
+      #       CaptivePortal = false;
+      #       DisableFirefoxStudies = true;
+      #       DisablePocket = true;
+      #       DisableTelemetry = true;
+      #       DisableFirefoxAccounts = false;
+      #       NoDefaultBookmarks = true;
+      #       OfferToSaveLogins = true;
+      #       OfferToSaveLoginsDefault = true;
+      #       PasswordManagerEnabled = true;
+      #       FirefoxHome = {
+      #         Search = true;
+      #         Pocket = false;
+      #         Snippets = false;
+      #         TopSites = false;
+      #         Highlights = false;
+      #       };
+      #       UserMessaging = {
+      #         ExtensionRecommendations = false;
+      #         SkipOnboarding = true;
+      #       };
+      #     };
+      #   };
+      #   profiles.default = {
+      #     name = "default";
+      #     path = "default";
+      #     id = 0;
+      #
+      #     extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      #       tridactyl
+      #       switchyomega
+      #     ];
+      #     isDefault = true;
+      #     search.default = "DuckDuckGo";
+      #     search.force = true;
+      #     settings = {
+      #       "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+      #     };
+      #   };
+      # };
     };
 
   };
@@ -102,7 +102,6 @@
     flameshot
     i3status
 
-    firefox
     xarchiver
     discord
     unstable.tdesktop
