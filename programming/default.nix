ac873fcdd44fce7b53d6d495f24b65708e66242d
ehsan <ehsan2003.2003.382@gmail.com>
2023-12-14 22:59:06 +0330

    adds virt-manager

--- programming/default.nix
+++ programming/default.nix
@@ -40,4 +40,15 @@
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "ehsan" ];
   virtualisation.docker.enable = true;
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
+  home-manager.users.ehsan = {
+    dconf.settings = {
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = [ "qemu:///system" ];
+        uris = [ "qemu:///system" ];
+      };
+    };
+  };
+  users.users.ehsan.extraGroups = [ "libvirtd" ];
 }
