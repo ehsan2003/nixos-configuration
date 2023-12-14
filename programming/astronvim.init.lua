d36da2b0251c11e8c20edea965ae39678917d6cb
ehsan <ehsan2003.2003.382@gmail.com>
2023-12-14 20:22:28 +0330

    adds vimtex

--- programming/astronvim.init.lua
+++ programming/astronvim.init.lua
@@ -1,16 +1,16 @@
 return {
 	-- Configure AstroNvim updates
 	updater = {
-		remote = "origin", -- remote to use
-		channel = "stable", -- "stable" or "nightly"
-		version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-		branch = "nightly", -- branch name (NIGHTLY ONLY)
-		commit = nil, -- commit hash (NIGHTLY ONLY)
-		pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
+		remote = "origin",   -- remote to use
+		channel = "stable",  -- "stable" or "nightly"
+		version = "latest",  -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+		branch = "nightly",  -- branch name (NIGHTLY ONLY)
+		commit = nil,        -- commit hash (NIGHTLY ONLY)
+		pin_plugins = nil,   -- nil, true, false (nil will pin plugins on stable only)
 		skip_prompts = false, -- skip prompts about breaking changes
 		show_changelog = true, -- show the changelog after performing an update
-		auto_quit = false, -- automatically quit the current session after a successful update
-		remotes = { -- easily add new remotes to track
+		auto_quit = false,   -- automatically quit the current session after a successful update
+		remotes = {          -- easily add new remotes to track
 			--   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
 			--   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
 			--   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
@@ -89,6 +89,10 @@ return {
 		},
 	},
 	plugins = {
+		{
+			"lervag/vimtex",
+			lazy = false
+		},
 		"sigmasd/deno-nvim", -- add lsp plugin
 		{ "williamboman/mason-lspconfig.nvim", },
 		"jose-elias-alvarez/typescript.nvim",
@@ -105,15 +109,15 @@ return {
 			lazy = false,
 		},
 		{
-  		"nvim-neo-tree/neo-tree.nvim",
-  		opts = {
-			enable_diagnostics = false,
-			filesystem = {
-				filtered_items = {
-					visible = true
+			"nvim-neo-tree/neo-tree.nvim",
+			opts = {
+				enable_diagnostics = false,
+				filesystem = {
+					filtered_items = {
+						visible = true
+					}
 				}
-			}
-  		},
+			},
 		}
 	},
 	mappings = {
