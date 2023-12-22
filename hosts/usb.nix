cbd9733f2b110295d9bda3f40d4ebc6b2d7e8e32
ehsan <ehsan2003.2003.382@gmail.com>
2023-12-22 20:21:55 +0330

    more optimization on usb

--- hosts/usb.nix
+++ hosts/usb.nix
@@ -8,11 +8,13 @@
 
   nix.optimise.automatic = true;
   nix.optimise.dates=["12:00"];
-  nix.gc.options = "--delete-older-than 7d";
+  nix.gc.options = lib.mkForce "--delete-older-than 7d";
+  boot.loader.grub.useOSProber=lib.mkForce false;
   services.xserver.desktopManager.xfce.enable = lib.mkForce false;
   boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
   boot.loader.grub.efiInstallAsRemovable = true;
   networking.hostName = "nixos-usb"; # Define your hostname.
+  environment.systemPackages=[ pkgs.xfce.thunar ];
   # boot.initrd.kernelModules = [ "uat" ];
   # checkout the example folder for how to configure different disko layouts
 
