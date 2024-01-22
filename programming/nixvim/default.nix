396d95d46e44e1a57d84c53339ea3a08c584d5cb
ehsan <ehsan2003.2003.382@gmail.com>
2024-01-22 21:44:02 +0330

    still working on lsp

--- programming/nixvim/default.nix
+++ programming/nixvim/default.nix
@@ -1,5 +1,6 @@
 { pkgs, fenix, ... }:
 {
+  imports = [ ./lsp.nix ];
   config = {
     plugins.bufferline.enable = true;
     plugins.markdown-preview.enable = true;
@@ -10,6 +11,8 @@
     colorschemes.tokyonight.enable = true;
     globals.mapleader = " ";
     plugins.neo-tree.enable = true;
+    plugins.neo-tree.filesystem.filteredItems.hideHidden = false;
+
     options.shiftwidth = 2; # Tab width should be 2
     options.expandtab = true; # Tab width should be 2
     options.tabstop = 2; # Tab width should be 2
@@ -19,6 +22,7 @@
     options.signcolumn = "yes:1";
     options.cursorline = true;
 
+    plugins.noice.enable = true;
 
     plugins.nvim-autopairs.enable = true;
     plugins.auto-save.enable = true;
@@ -26,12 +30,6 @@
     plugins.comment-nvim.toggler.line = "<leader>/";
     extraPlugins = with pkgs.vimPlugins ; [ smart-splits-nvim friendly-snippets ];
     plugins.toggleterm.enable = true;
-    plugins.luasnip.enable = true;
-    plugins.luasnip.fromVscode = [
-      { }
-    ];
-
-    plugins.cmp_luasnip.enable = true;
     plugins.auto-session.enable = true;
     plugins.lualine.enable = true;
     plugins.telescope = {
@@ -47,84 +45,7 @@
         };
       };
     };
-
-    plugins.lspkind.enable = true;
-    plugins.lspkind.mode = "symbol";
-    plugins.nvim-cmp = {
-      enable = true;
-      sources = [
-        { name = "nvim_lsp"; priority = 1000; }
-        { name = "luasnip"; priority = 750; }
-        {
-          name = "buffer";
-          priority = 500;
-          option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
-        }
-        { name = "path"; priority = 250; }
-      ];
-
-      window = {
-        completion = {
-          border = "rounded";
-          winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None";
-          scrolloff = 0;
-          colOffset = 0;
-          sidePadding = 1;
-          scrollbar = true;
-        };
-        documentation = {
-          maxHeight = "math.floor(40 * (40 / vim.o.lines))";
-          maxWidth = "math.floor((40 * 2) * (vim.o.columns / (40 * 2 * 16 / 9)))";
-          border = "rounded";
-          winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None";
-        };
-      };
-
-      snippet.expand = "luasnip";
-      mapping = {
-        "<CR>" = "cmp.mapping.confirm({ select = false })";
-        "<C-u>" = ''cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" })'';
-        "<C-d>" = ''cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" })'';
-        "<C-Space>" = ''cmp.mapping.complete()'';
-        "<S-Tab>" = {
-          modes = [ "i" "s" ];
-          action = ''
-            function(fallback)
-              local luasnip=require("luasnip")
-              if cmp.visible() then
-                cmp.select_prev_item()
-              elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-              else
-                fallback()
-              end
-            end 
-          '';
-        };
-        "<Tab>" = {
-          modes = [ "i" "s" ];
-          action = ''
-            function(fallback)
-              local luasnip=require("luasnip")
-              if cmp.visible() then
-                cmp.select_next_item()
-              elseif luasnip.expandable() then
-                luasnip.expand()
-              elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-              else
-                fallback()
-              end
-            end
-          '';
-        };
-      };
-    };
-    plugins.cmp-nvim-lsp.enable = true;
-    plugins.lsp-lines.enable = true;
     plugins.gitsigns.enable = true;
-
-
     plugins.treesitter = {
       enable = true;
       incrementalSelection.enable = true;
@@ -132,25 +53,7 @@
       incrementalSelection.keymaps.nodeDecremental = "<A-l>";
       incrementalSelection.keymaps.nodeIncremental = "<A-h>";
     };
-    plugins.lsp = {
-      enable = true;
-      servers.tsserver.enable = true;
 
-      servers.rust-analyzer = {
-        enable = true;
-        cargoPackage = fenix.stable.cargo;
-        rustcPackage = fenix.stable.rustc;
-        installCargo = false;
-        installRustc = false;
-      };
-      servers.html.enable = true;
-      servers.tailwindcss.enable = true;
-      servers.svelte.enable = true;
-      servers.emmet_ls.enable = true;
-      servers.pyright.enable = true;
-      servers.ccls.enable = true;
-      servers.rnix-lsp.enable = true;
-    };
 
 
     keymaps = [
@@ -176,27 +79,7 @@
         action = "<Cmd>ToggleTerm size=10 direction=horizontal<CR>";
         mode = "n";
       }
-      {
-        key = "K";
-        action = "function () vim.lsp.buf.hover() end";
-        options.desc = "Hover symbol details";
-        lua = true;
-        mode = "n";
-      }
-      {
-        key = "<leader>lr";
-        action = "function () vim.lsp.buf.rename() end";
-        options.desc = "Rename using lsp";
-        lua = true;
-        mode = "n";
-      }
-      {
-        key = "<leader>lf";
-        action = "function () vim.lsp.buf.format() end";
-        options.desc = "Format using lsp";
-        lua = true;
-        mode = "n";
-      }
+
       {
         key = "<leader>tv";
         action = "<Cmd>ToggleTerm size=80 direction=vertical<CR>";
---
+++ programming/nixvim/lsp.nix
@@ -0,0 +1,205 @@
+{ pkgs, fenix, ... }:
+{
+  plugins = {
+    luasnip.enable = true;
+    luasnip.fromVscode = [
+      { }
+    ];
+
+    cmp_luasnip.enable = true;
+
+    lspkind.enable = true;
+    lspkind.mode = "symbol";
+    nvim-cmp = {
+      enable = true;
+      sources = [
+        { name = "nvim_lsp"; priority = 1000; }
+        { name = "luasnip"; priority = 750; }
+        {
+          name = "buffer";
+          priority = 500;
+          option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+        }
+        { name = "path"; priority = 250; }
+      ];
+
+      window = {
+        completion = {
+          border = "rounded";
+          winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None";
+          scrolloff = 0;
+          colOffset = 0;
+          sidePadding = 1;
+          scrollbar = true;
+        };
+        documentation = {
+          maxHeight = "math.floor(40 * (40 / vim.o.lines))";
+          maxWidth = "math.floor((40 * 2) * (vim.o.columns / (40 * 2 * 16 / 9)))";
+          border = "rounded";
+          winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None";
+        };
+      };
+
+      snippet.expand = "luasnip";
+      mapping = {
+        "<CR>" = "cmp.mapping.confirm({ select = false })";
+        "<C-u>" = ''cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" })'';
+        "<C-d>" = ''cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" })'';
+        "<C-Space>" = ''cmp.mapping.complete()'';
+        "<S-Tab>" = {
+          modes = [ "i" "s" ];
+          action = ''
+            function(fallback)
+              local luasnip=require("luasnip")
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end 
+          '';
+        };
+        "<Tab>" = {
+          modes = [ "i" "s" ];
+          action = ''
+            function(fallback)
+              local luasnip=require("luasnip")
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.expandable() then
+                luasnip.expand()
+              elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+              else
+                fallback()
+              end
+            end
+          '';
+        };
+      };
+    };
+    cmp-nvim-lsp.enable = true;
+    lsp-lines.enable = true;
+    lsp = {
+      enable = true;
+      servers.tsserver.enable = true;
+
+      servers.rust-analyzer = {
+        enable = true;
+        cargoPackage = fenix.stable.cargo;
+        rustcPackage = fenix.stable.rustc;
+        installCargo = false;
+        installRustc = false;
+      };
+      servers.html.enable = true;
+      servers.tailwindcss.enable = true;
+      servers.svelte.enable = true;
+      servers.emmet_ls.enable = true;
+      servers.pyright.enable = true;
+      servers.ccls.enable = true;
+      servers.rnix-lsp.enable = true;
+    };
+  };
+  keymaps = [
+    {
+      key = "gd";
+      action = ''function() require("telescope.builtin").lsp_definitions() end'';
+      options.desc = "Go to definition";
+      lua = true;
+      mode = "n";
+    }
+    {
+      key = "gr";
+      action = ''function() require("telescope.builtin").lsp_references() end'';
+      options.desc = "References of current symbol";
+      lua = true;
+      mode = "n";
+    }
+    {
+      key = "<leader>lR";
+      action = ''function() require("telescope.builtin").lsp_references() end'';
+      options.desc = "Search references";
+      lua = true;
+      mode = "n";
+    }
+    {
+      key = "<leader>lr";
+      action = "function() vim.lsp.buf.rename() end";
+      options.desc = "Rename current symbol";
+      lua = true;
+      mode = "n";
+    }
+    {
+      key = "<leader>lh";
+      action = "function() vim.lsp.buf.signature_help() end";
+      options.desc = "Signature help";
+      lua = true;
+      mode = "n";
+    }
+    {
+      key = "gy";
+      action = ''function() require("telescope.builtin").lsp_type_definitions() end'';
+      options.desc = "Definition of current type";
+      lua = true;
+      mode = "n";
+    }
+    {
+      key = "<leader>lG";
+      action = "function() vim.lsp.buf.workspace_symbol() end";
+      options.desc = "Search workspace symbols";
+      lua = true;
+      mode = "n";
+    }
+    {
+      key = "K";
+      action = "function () vim.lsp.buf.hover() end";
+      options.desc = "Hover symbol details";
+      lua = true;
+      mode = "n";
+    }
+    {
+      key = "<leader>lr";
+      action = "function () vim.lsp.buf.rename() end";
+      options.desc = "Rename using lsp";
+      lua = true;
+      mode = "n";
+    }
+    {
+      key = "<leader>ld";
+      action = "function() vim.diagnostic.open_float() end";
+      options.desc = "Hover diagnostics";
+      lua = true;
+      mode = "n";
+    }
+    {
+      key = "[d";
+      action = "function() vim.diagnostic.goto_prev() end";
+      options.desc = "Previous diagnostic";
+      lua = true;
+      mode = "n";
+    }
+    {
+      key = "]d";
+      action = "function() vim.diagnostic.goto_next() end";
+      options.desc = "Next diagnostic";
+      lua = true;
+      mode = "n";
+    }
+    {
+      key = "gl";
+      action = "function() vim.diagnostic.open_float() end";
+      options.desc = "Hover diagnostics";
+      lua = true;
+      mode = "n";
+    }
+    {
+      key = "<leader>lf";
+      action = "function () vim.lsp.buf.format() end";
+      options.desc = "Format using lsp";
+      lua = true;
+      mode = "n";
+    }
+  ];
+}
