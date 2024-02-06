d5203f31699f529c079bbae1862435ab730fd7bd
ehsan <ehsan2003.2003.382@gmail.com>
2024-02-06 14:08:09 +0330

    adds openssl

--- cli/default.nix
+++ cli/default.nix
@@ -14,6 +14,7 @@ in {
   environment.systemPackages = with pkgs; [
     # editors
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    openssl
 
     # Netowrk
     wget
