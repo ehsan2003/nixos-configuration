1963a209ebb6edb048b26f5bec960fed471bd6c1
ehsan <ehsan2003.2003.382@gmail.com>
2024-02-18 17:20:20 +0330

    uses nix file instead of json file for secrets

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
           system = "x86_64-linux";
           specialArgs = inputs // {
             unstable = inputs.unstable.legacyPackages.${system};
-            secrets = nixpkgs.lib.importJSON /etc/secrets.json;
+            secrets = import /etc/secrets.nix;
           };
         in
         {
