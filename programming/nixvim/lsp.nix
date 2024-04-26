0927bc0971ff12a4a35cae37e8d655dbeef9c35f
ehsan <ehsan2003.2003.382@gmail.com>
2024-04-26 09:16:01 +0330

    fixes luasnip

--- programming/nixvim/lsp.nix
+++ programming/nixvim/lsp.nix
@@ -54,7 +54,8 @@
               end
           '';
         };
-        snippet.expand = "luasnip";
+        snippet.expand =
+          "function(args) require('luasnip').lsp_expand(args.body) end";
         sources = [
           {
             name = "nvim_lsp";
