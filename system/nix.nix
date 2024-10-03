0b14ce028637a357274005de6fcd3bbeea7a9b40
ehsan <ehsan2003.2003.382@gmail.com>
2024-10-03 15:51:46 +0330

    added proxy to nix daemon

--- system/nix.nix
+++ system/nix.nix
@@ -18,4 +18,9 @@
     persistent = true;
     options = "--delete-older-than 30d";
   };
+  systemd.services.nix-daemon.environment = {
+    # socks5h mean that the hostname is resolved by the SOCKS server
+    https_proxy = "socks5h://localhost:1081";
+    # https_proxy = "http://localhost:7890"; # or use http prctocol instead of socks5
+  };
 }
