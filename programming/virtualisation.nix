af47d10be88a986650ad89b0f51fc3f6367d42c9
ehsan <ehsan2003.2003.382@gmail.com>
2024-10-20 08:50:05 +0330

    virtualization

--- programming/virtualisation.nix
+++ programming/virtualisation.nix
@@ -2,5 +2,16 @@
   virtualisation.docker.enable = true;
   virtualisation.docker.daemon.settings.registry-mirrors =
     [ "https://registry.docker.ir" ];
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
+  home-manager.users.ehsan = {
+    dconf.settings = {
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = [ "qemu:///system" ];
+        uris = [ "qemu:///system" ];
+      };
+    };
+  };
+  users.users.ehsan.extraGroups = [ "libvirtd" ];
 
 }
