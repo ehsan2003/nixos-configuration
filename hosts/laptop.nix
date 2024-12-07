3a6d814b84efa75b8ff0b6bc46135f20704cf76d
ehsan <ehsan2003.2003.382@gmail.com>
2024-12-07 10:34:57 +0330

    added dpi settings for laptop

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -1,6 +1,15 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let dpi = 120;
+in {
   imports = [ ./base.nix ];
   networking.hostName = "nixos-laptop"; # Define your hostname.
+  services.xserver.dpi = dpi;
+  home-manager.users.ehsan.programs.rofi.extraConfig."dpi" = dpi;
+  services.xserver.displayManager.sessionCommands = ''
+    ${pkgs.xorg.xrdb}/bin/xrdb -merge <<EOF
+      Xft.dpi: ${toString dpi}  
+    EOF
+  '';
 
   services.tlp.enable = true;
 
