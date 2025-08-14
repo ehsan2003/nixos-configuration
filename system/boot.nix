60c9ffd3c64c32313669f3cd251197d462d00314
ehsan <ehsan2003.2003.382@gmail.com>
2025-08-14 11:44:22 +0330

    upgraded the system

--- system/boot.nix
+++ system/boot.nix
@@ -7,7 +7,5 @@
   boot.loader.grub.device = "nodev";
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.useOSProber = true;
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_13;
-  boot.extraModulePackages = [ pkgs.linuxKernel.packages.linux_6_13.amneziawg ];
 
 }
