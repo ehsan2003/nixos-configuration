adeeaceffe23a45d420ce8240e88341951f959fe
ehsan <ehsan2003.2003.382@gmail.com>
2025-09-13 10:11:59 +0330

    added avante again

--- programming/nixvim/ai.nix
+++ programming/nixvim/ai.nix
@@ -4,13 +4,28 @@ in {
   plugins.avante = {
     enable = true;
     settings = {
-      openai = {
-        endpoint = "https://api.groq.com/openai/v1";
-        max_tokens = 4096;
-        api_key_name = "GROQ_API_KEY";
-        model = "llama-3.3-70b-versatile";
-        proxy = "socks5://localhost:1080";
-        temperature = 0;
+      providers = {
+        ossfast = {
+          __inherited_from = "openai";
+          endpoint = "https://openrouter.ai/api/v1";
+          api_key_name = "OPENROUTER_API_KEY";
+          model = "openai/gpt-oss-120b:nitro";
+        };
+        gpt5 = {
+          __inherited_from = "openai";
+          endpoint = "https://openrouter.ai/api/v1";
+          api_key_name = "OPENROUTER_API_KEY";
+          model = "openai/gpt-5";
+        };
+        groqoss = {
+          __inherited_from = "openai";
+          api_key_name = "GROQ_API_KEY";
+          endpoint = "https://api.groq.com/openai/v1/";
+          model = "openai/gpt-oss-120b";
+          proxy = "socks5://localhost:1080";
+          max_tokens = 4096;
+          temperature = 0;
+        };
       };
       provider = "openai";
       diff = {
