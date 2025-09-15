25b2a28d6c931f80b8e03503de65f76df6e79ea7
ehsan <ehsan2003.2003.382@gmail.com>
2025-09-15 09:46:54 +0330

    removed cuda support from home pc

--- hosts/home-pc.nix
+++ hosts/home-pc.nix
@@ -5,7 +5,7 @@
   environment.systemPackages = [
 
   ];
-  services.serviio.enable = true;
+  # services.serviio.enable = true;
   systemd.services.task-sync = {
     enable = true;
     description = "taskwarrior sync server";
@@ -61,6 +61,6 @@
     # Compare to the key published at https://nix-community.org/cache
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
   ];
-  nixpkgs.config.cudaSupport = true;
+  # nixpkgs.config.cudaSupport = true;
 
 }
