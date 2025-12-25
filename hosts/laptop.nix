8ffa30ba64a49d16a1ea07f6df92ae8fd3e802d0
ehsan <ehsan2003.2003.382@gmail.com>
2025-12-26 00:35:13 +0330

    fix: fixed syntax error

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -17,7 +17,7 @@ in {
     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
     CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-    CPU_EtERGY_PERF_POLICY_ON_AC = "performance";
+    CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
   };
 }
