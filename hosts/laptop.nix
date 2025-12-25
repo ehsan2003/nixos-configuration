0eba1989196c0f09b4125068c9ae48c6e63bbf49
ehsan <ehsan2003.2003.382@gmail.com>
2025-12-26 00:29:24 +0330

    feat: better power management

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -12,4 +12,12 @@ in {
   '';
 
   services.tlp.enable = true;
+  services.tlp.settings = {
+    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+    CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+    CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+  }
 }
--- hosts/new-laptop.nix
+++ hosts/new-laptop.nix
@@ -13,5 +13,13 @@ in {
   '';
 
   services.tlp.enable = true;
+  services.tlp.settings = {
+    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+    CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+    CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+  }
 }
 
