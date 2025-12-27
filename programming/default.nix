85aead2898e4f2ae0891537a2797f7644466416e
ehsan <ehsan2003.2003.382@gmail.com>
2025-12-27 22:15:19 +0330

    feat: added claude code mcp stuff and switched to hm module

--- programming/default.nix
+++ programming/default.nix
@@ -48,8 +48,11 @@ in {
     ])
     pre-commit
   ];
-  home-manager.users.ehsan.home.file.claude-settinngs = {
-    text = builtins.toJSON {
+
+  home-manager.users.ehsan.programs.claude-code = {
+    enable = true;
+    package = unstable.claude-code;
+    settings = {
       env = {
         ANTHROPIC_DEFAULT_HAIKU_MODEL = "glm-4.7";
         ANTHROPIC_DEFAULT_SONNET_MODEL = "glm-4.7";
@@ -79,7 +82,39 @@ in {
       };
       alwaysThinkingEnabled = true;
     };
-    target = ".claude/settings.json";
+    mcpServers = {
+      "web-search-prime" = {
+        "type" = "http";
+        "url" = "https://api.z.ai/api/mcp/web_search_prime/mcp";
+        "headers" = {
+          "Authorization" = "Bearer ${secrets.ANTHROPIC_AUTH_TOKEN}";
+        };
+      };
+
+      "zai-mcp-server" = {
+        "type" = "stdio";
+        "command" = "npx";
+        "args" = [ "-y" "@z_ai/mcp-server" ];
+        "env" = {
+          "Z_AI_API_KEY" = "${secrets.ANTHROPIC_AUTH_TOKEN}";
+          "Z_AI_MODE" = "ZAI";
+        };
+      };
+      "web-reader" = {
+        "type" = "http";
+        "url" = "https://api.z.ai/api/mcp/web_reader/mcp";
+        "headers" = {
+          "Authorization" = "Bearer ${secrets.ANTHROPIC_AUTH_TOKEN}";
+        };
+      };
+      "zread" = {
+        "type" = "http";
+        "url" = "https://api.z.ai/api/mcp/zread/mcp";
+        "headers" = {
+          "Authorization" = "Bearer ${secrets.ANTHROPIC_AUTH_TOKEN}";
+        };
+      };
+    };
   };
   services.postgresql.package = pkgs.postgresql_17;
   services.pgadmin.enable = true;
