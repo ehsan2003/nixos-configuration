e085b15854af61545fd5ce5d77e994e5a74f5b0b
Ehsan <ehsan2003.2003.382@gmail.com>
2026-02-07 19:08:58 +0330

    feat: improved nvim navigation with cybu

--- programming/nixvim/navigation.nix
+++ programming/nixvim/navigation.nix
@@ -2,6 +2,70 @@
 {
 
   plugins.smart-splits.enable = true;
+  plugins.cybu.enable = true;
+  plugins.cybu.settings = {
+    position = {
+      relative_to = "win"; # win, editor, cursor
+      anchor = "topcenter"; # topleft, topcenter, topright,
+      # centerleft, center, centerright,
+      # bottomleft, bottomcenter, bottomright
+      vertical_offset = 10; # vertical offset from anchor in lines
+      horizontal_offset = 0; # vertical offset from anchor in columns
+      max_win_height = 5; # height of cybu window in lines
+      max_win_width = 0.5; # integer for absolute in columns
+      # float for relative to win/editor width
+    };
+    style = {
+      path = "relative"; # absolute, relative, tail (filename only),
+      # tail_dir (filename & parent dir)
+      path_abbreviation = "none"; # none, shortened
+      border = "rounded"; # single, double, rounded, none
+      separator = " "; # string used as separator
+      prefix = "…"; # string used as prefix for truncated paths
+      padding = 1; # left & right padding in number of spaces
+      hide_buffer_id = true; # hide buffer IDs in window
+      devicons = {
+        enabled = true; # enable or disable web dev icons
+        colored = true; # enable color for web dev icons
+        truncate = true; # truncate wide icons to one char width
+      };
+      highlights = {
+        # see highlights via :highlight
+        current_buffer = "CybuFocus"; # current / selected buffer
+        adjacent_buffers = "CybuAdjacent"; # buffers not in focus
+        background = "CybuBackground"; # window background
+        border = "CybuBorder"; # border of the window
+      };
+    };
+    behavior = {
+      # set behavior for different modes
+      mode = {
+        default = {
+          switch = "on_close"; # immediate, on_close
+          view = "rolling"; # paging, rolling
+        };
+        last_used = {
+          switch = "on_close"; # immediate, on_close
+          view = "rolling"; # paging, rolling
+          update_on = "buf_enter"; # buf_enter, cursor_moved
+        };
+        auto = {
+          view = "rolling"; # paging, rolling
+        };
+      };
+      show_on_autocmd = false; # event to trigger cybu (eg. "BufEnter")
+    };
+    display_time = 300; # time the cybu window is displayed
+    exclude = [
+      # filetypes, cybu will not be active
+      "neo-tree"
+      "fugitive"
+      "qf"
+    ];
+    filter = {
+      unlisted = true; # filter & fallback for unlisted buffers
+    };
+  };
   keymaps = [
     {
       # Default mode is "" which means normal-visual-op
@@ -26,12 +90,12 @@
     }
     {
       key = "H";
-      action = "<Cmd>bprevious<Cr>";
+      action = "<Plug>(CybuLastusedPrev)";
       mode = "n";
     }
     {
       key = "L";
-      action = "<Cmd>bnext<Cr>";
+      action = "<Plug>(CybuLastusedNext)";
       mode = "n";
     }
     {
